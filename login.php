f8afb6cc792845c522edd0423e873b126b41763b
lewee aaron ombac <laombac14@gmail.com>
2024-11-02 02:27:30 +0800

    sample login

---
+++ login.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="modern-normalize.css">
+    <link rel="stylesheet" href="utils.css">
+    <link rel="stylesheet" href="login.css">
+    <title>Apt-Ease | Login</title>
+</head>
+<body>
+<div class="login__wrapper">
+    <div class="login__card">
+        <div class="login__image">
+            <img class="aptease" src="./src/apt-ease.jpg" alt="AptEase Logo">
+        </div>
+        <div class="login__form">
+            <div class="login__information">
+                <h1>Welcome, User!</h1>
+                <p>Sign in to continue</p>
+            </div>
+            <div class="login__inputs">
+                <form class="login__creds" action="">
+                    <input type="text" placeholder="Username" name="username" id="username">
+                    <input type="password" placeholder="Password" name="password" id="password">
+                    
+                </form>
+                <button type="submit">Login</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
