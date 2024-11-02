6f16d256f57ba82753b5bac418e10b8c658529e5
lewee aaron ombac <laombac14@gmail.com>
2024-11-02 23:09:14 +0800

    added main

---
+++ main.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="modern-normalize.css">
+    <link rel="stylesheet" href="utils.css">
+    <link rel="stylesheet" href="main.css">
+
+    <title>Main | AptEase</title>
+</head>
+<body>
+    <div class="main__wrapper">
+        <div class="main__sidebar">
+            <div class="main__user">
+                <span class="close">Close</span>
+                <h1>Welcome, User!</h1>
+            </div>
+            <div class="main__sidebarlinks">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Accounts</a>
+                            <a href="#">Tenants</a>
+                            <a href="#">Payment History</a>
+                            <a href="#">Maintentance Records</a>
+                            <a href="#">Maintentance Submission</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        
+        </div>
+        <div class="main__middlecontent">
+            
+        </div>
+        <div class="main__footer">
+            <footer>
+
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
