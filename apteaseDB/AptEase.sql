3f97ebcbd199a5fce4ac6204fc5576866a1b117d
Amos Ombac <ombac@Ombacs-MacBook-Air.local>
2025-02-22 19:50:57 +0800

    aptease databse

---
+++ apteaseDB/AptEase.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 22, 2025 at 12:46 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `AptEase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblaccounts`
+--
+
+CREATE TABLE `tblaccounts` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `usertype` varchar(20) NOT NULL,
+  `createdby` varchar(30) NOT NULL,
+  `datecreated` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblaccounts`
+--
+
+INSERT INTO `tblaccounts` (`username`, `password`, `usertype`, `createdby`, `datecreated`) VALUES
+('tryadmin', 'tryadmin', 'ADMIN', 'ADMIN', '22/02/25');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblaccounts`
+--
+ALTER TABLE `tblaccounts`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
