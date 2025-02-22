0b9c7b9a2aa846e86304580df7591fdd554628fc
Ombac <ombac@Ombacs-MacBook-Air.local>
2025-02-22 19:10:29 +0800

    UTEN

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
-
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'aptease');
-define('DB_PASSWORD', '123');
-define('DB_NAME', 'aptease');
-
-
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-
-if($link === false){
-  
-    die("Error: Could not connect. " . mysqli_connect_error());
+    // Define database connection constants
+    define('DB_SERVER', '127.0.0.1');
+    define('DB_USERNAME', 'aptease');
+    define('DB_PASSWORD', 'aptease123');
+    define('DB_NAME', 'AptEase');
+    // Attempt to connect to the database
+    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    // Check if the connection is unsuccessful
+    if($link === false) {
+        die("ERROR: Could not connect. " . mysqli_connect_error());
     }
-    date_default_timezone_set('Asia/Manila')
-    
+    // Set time zone
+    date_default_timezone_set('Asia/Manila');
 ?>
